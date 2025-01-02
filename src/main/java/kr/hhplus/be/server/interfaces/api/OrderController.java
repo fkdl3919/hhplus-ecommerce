89ab5912882ba55cef5c530c760c89057a3f2d3f
gil <fkdl3919@gmail.com>
2025-01-03 02:44:09 +0900

    fix: order url 수정

--- src/main/java/kr/hhplus/be/server/interfaces/api/OrderController.java
+++ src/main/java/kr/hhplus/be/server/interfaces/api/OrderController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/product")
+@RestController("/api/v1/order")
 public class OrderController {
 
 
