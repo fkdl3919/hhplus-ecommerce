45a0ae4e2604e4c8920d2c393eba19cf5ec889f2
gil <fkdl3919@gmail.com>
2025-01-03 02:12:36 +0900

    chore: 롬복 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
+	compileOnly ("org.projectlombok:lombok")
+
+
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
