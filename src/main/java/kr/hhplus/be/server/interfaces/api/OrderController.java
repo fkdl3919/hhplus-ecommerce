d084eb964b2938b1fd2e2c46903bc5d6278c422d
gil <fkdl3919@gmail.com>
2025-01-03 02:24:06 +0900

    feat: ordercontroller 추가

---
+++ src/main/java/kr/hhplus/be/server/interfaces/api/OrderController.java
@@ -0,0 +1,11 @@
+package kr.hhplus.be.server.interfaces.api;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/product")
+public class OrderController {
+
+
+}
