3626e4ccf90f31fb77a87c5a157d9c283748fa56
gil <fkdl3919@gmail.com>
2025-01-10 01:18:58 +0900

    chore: querydsl 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,11 @@ dependencies {
 	// api docs
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
 
+	// queryDSL
+	implementation ("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	annotationProcessor ("com.querydsl:querydsl-apt:5.0.0:jakarta")
+	annotationProcessor ("jakarta.annotation:jakarta.annotation-api")
+	annotationProcessor ("jakarta.persistence:jakarta.persistence-api")
 
 }
 
