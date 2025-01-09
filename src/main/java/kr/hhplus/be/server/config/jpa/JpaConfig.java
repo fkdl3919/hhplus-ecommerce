d2a796989311322cbeac0117e295d99247e370e8
gil <fkdl3919@gmail.com>
2025-01-10 01:14:51 +0900

    feat: jpaConfig 생성

--- src/main/java/kr/hhplus/be/server/config/jpa/JpaConfig.java
+++ src/main/java/kr/hhplus/be/server/config/jpa/JpaConfig.java
@@ -1,5 +1,8 @@
 package kr.hhplus.be.server.config.jpa;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -15,5 +18,10 @@ public class JpaConfig {
     @Bean
     public PlatformTransactionManager transactionManager() {
         return new JpaTransactionManager();
+    }
+
+    @Bean
+    public JPAQueryFactory queryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
     }
 }
